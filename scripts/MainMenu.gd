5b1abd38ea8e0923775144ce7c3048e1b2a7b8b0
explosiver1 <jrdholston@gmail.com>
2024-05-14 17:03:16 -0500

    Added scenes and scripts folder before the merge with Ring. Also added a simple main menu.

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	var scene = load("res://scenes/level1.tscn")
+	get_tree().change_scene_to_packed(scene)
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
